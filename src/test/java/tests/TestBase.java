1ab992c4e402a7e721985ed19bf5e7e410982464
Amrik Samra <amriksmr@gmail.com>
2025-10-12 18:18:41 -0700

    uncommented code

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -103,18 +103,18 @@ public class TestBase {
         AppiumUtil.app.closeApp();
     }
 
-//    @AfterSuite
-//    public void afterSuite() {
-//        Path inno = Paths.get(System.getenv("LOCALAPPDATA"), "Shift", "unins000.exe");
-//
-//        String cmd = "Start-Process -FilePath '" + inno + "' -ArgumentList '/VERYSILENT','/SUPPRESSMSGBOXES','/NORESTART' -Wait";
-//        String result = new PowerShellUtil().exec(cmd);
-//        Assert.assertEquals(result, "", "Shift never uninstalled");
-//        logger.info("Shift uninstalled successfully ");
-//
-//        this.deleteInstallationFilesInDownloads(this.shiftFiles);
-//    }
-//
+    @AfterSuite
+    public void afterSuite() {
+        Path inno = Paths.get(System.getenv("LOCALAPPDATA"), "Shift", "unins000.exe");
+
+        String cmd = "Start-Process -FilePath '" + inno + "' -ArgumentList '/VERYSILENT','/SUPPRESSMSGBOXES','/NORESTART' -Wait";
+        String result = new PowerShellUtil().exec(cmd);
+        Assert.assertEquals(result, "", "Shift never uninstalled");
+        logger.info("Shift uninstalled successfully ");
+
+        this.deleteInstallationFilesInDownloads(this.shiftFiles);
+    }
+
     private void deleteInstallationFilesInDownloads(File[] path) {
         if (path != null) {
             for (File file : path) {
